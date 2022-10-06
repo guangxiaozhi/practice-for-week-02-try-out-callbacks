717cab98eff420bbfd51a25d34b7600494ba9cdc
Xiaoguang Wang <guangxiaozhi@hotmail.com>
2022-10-06 00:19:53 -0700

    24

--- problems/24-my-simple-reduce.js
+++ problems/24-my-simple-reduce.js
@@ -20,6 +20,7 @@ let result2 = mySimpleReduce([4, 6, 2], function(product, el) {
 });
 console.log(result2); // 48
 
+
 let result3 = mySimpleReduce([4, 6, 2, 8, 3], function(max, el) {
     if (el > max) {
         return el;
